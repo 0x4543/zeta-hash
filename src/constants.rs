c8b8251157c1272df2f8acb10b960c632ecae6a2
0x4543 <intodeadsilence@gmail.com>
2025-12-20 14:33:30 -0500

    refactor: add BUFFER_SIZE constant

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,5 @@
 pub const SALT_CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                                  abcdefghijklmnopqrstuvwxyz\
                                  0123456789";
+
+pub const BUFFER_SIZE: usize = 4096;
