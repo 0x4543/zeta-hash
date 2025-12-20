1d1b13de35725fe6a2b3aff5c5807426b37cd242
0x4543 <intodeadsilence@gmail.com>
2025-12-20 14:54:53 -0500

    chore: register types module and re-export Algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ pub mod constants;
 pub mod file_hasher;
 pub mod random_salt;
 pub mod string_hasher;
+pub mod types;
 
 pub use file_hasher::FileHasher;
 pub use random_salt::generate_salt;
 pub use string_hasher::{hash_sha256, hash_keccak256, hash_blake3};
+pub use types::Algorithm;
