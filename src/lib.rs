189494797d5f63c950d3bd1e92c45eeca336890a
0x4543 <intodeadsilence@gmail.com>
2025-12-20 14:00:46 -0500

    refactor: clean up lib.rs and re-export modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub mod file_hasher;
 pub mod random_salt;
+pub mod string_hasher;
 
-pub use file_hasher::{hash_sha256, hash_keccak256, hash_blake3, FileHasher};
+pub use file_hasher::FileHasher;
 pub use random_salt::generate_salt;
+pub use string_hasher::{hash_sha256, hash_keccak256, hash_blake3};
