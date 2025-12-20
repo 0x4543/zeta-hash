140eac6892c4779794058816796231c125cdbda0
0x4543 <intodeadsilence@gmail.com>
2025-12-20 14:58:13 -0500

    chore: register error module

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 pub mod args;
 pub mod constants;
+pub mod error;
 pub mod file_hasher;
 pub mod random_salt;
 pub mod string_hasher;
 pub mod types;
 
+pub use error::ZetaError;
 pub use file_hasher::FileHasher;
 pub use random_salt::generate_salt;
 pub use string_hasher::{hash_sha256, hash_keccak256, hash_blake3};
