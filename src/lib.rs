614525f52c2c6cfdff91f7d76deae4762956c4fa
0x4543 <intodeadsilence@gmail.com>
2025-12-20 15:00:29 -0500

    chore: register app module and export run function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod app;
 pub mod args;
 pub mod constants;
 pub mod error;
@@ -6,6 +7,7 @@ pub mod random_salt;
 pub mod string_hasher;
 pub mod types;
 
+pub use app::run;
 pub use error::ZetaError;
 pub use file_hasher::FileHasher;
 pub use random_salt::generate_salt;
